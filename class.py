3a2626097d3361544711dacf2292c34f1c7b6909
TaahaTm <taahatahir.m@gmail.com>
2026-08-04 14:35:28 +0300

    Done

---
+++ class.py
@@ -0,0 +1,20 @@
+class Lesson:
+    def __init__(self, LessonType, Instructor):
+        self.__LessonType = LessonType
+        self.__Instructor = Instructor
+
+    def GetLessonType(self):
+        return self.__LessonType
+
+    def GetFree(character):
+        if character == "B":
+            return "$45"
+        elif character == "I":
+            return "$50"
+        elif character == "A":
+            return "$55"
+        else:
+            return -1
+
+    LessonArray = [' '] * 3
+    LessonArray[2] = Lesson("Improve Your Serve", "David")
