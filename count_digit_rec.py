813c12795e218020e1e7470129751465a94b0a76
TaahaTm <taahatahir.m@gmail.com>
2026-08-07 13:36:20 +0300

    Recursive-count

--- count_digit_rec.py
+++ count_digit_rec.py
@@ -19,8 +19,17 @@ def counting(number, count=0):
 
     if number <= 0 :
         return count
-    new = number % 10
+    #new = number % 10
+    #Sir this line ^ has no effect on the code
 
     return counting(number//10,  count + 1)
 
 print(counting(803199))
+
+# My ITERVATIVE METHOD
+num = 1234567890
+count1 = 0
+while num > 0:
+  num = num // 10
+  count1 += 1
+print(count1)
