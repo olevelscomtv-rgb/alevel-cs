0e00faeb6ae7525fa98636c1a49fb980e81f6ce2
TaahaTm <taahatahir.m@gmail.com>
2026-08-08 00:55:58 +0300

    Sort-check-recv

--- sorted_array_rec.py
+++ sorted_array_rec.py
@@ -9,3 +9,18 @@ def loop_sort_check(array):
     return True
 
 print(loop_sort_check(arr))
+
+#RECURSIVE FUNCTION
+
+def sort_check(array, count=0):
+    if count >= len(array)-1:
+        return True
+    
+    
+    if array[count] > array[count + 1]:
+        return False
+
+    return sort_check(array, count + 1)
+
+arr1 = [3, 10, 90, 10, 4, 50]
+print(sort_check(arr1))
