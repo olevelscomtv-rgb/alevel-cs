8cef083c06350354c54df47aa06821065b16f1b1
TaahaTm <taahatahir.m@gmail.com>
2026-08-11 13:37:08 +0300

    updated q3 part ai

--- Q3.py
+++ Q3.py
@@ -1,7 +1,8 @@
 class Excercise:
-    def __init__(self, Duration: int, CaloriesBurned: float):
+    def __init__(self, ExerciseType: str, Duration: int, CaloriesBurned: float):
+        self.ExerciseType = ExerciseType
         self.Duration = Duration
         self.CaloriesBurned = CaloriesBurned
 
-    def Constructor(self, Duration, CaloriesBurned = 0):
+    def Constructor(self, ExerciseType, Duration, CaloriesBurned = 0):
         pass
