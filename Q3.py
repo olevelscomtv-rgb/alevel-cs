909ac5136f608c2830240fd5e2bbdc5ef4ed2c6a
TaahaTm <taahatahir.m@gmail.com>
2026-08-11 13:47:54 +0300

    Q3 part b

--- Q3.py
+++ Q3.py
@@ -1,14 +1,25 @@
 class Excercise:
-    def __init__(self, ExerciseType: str, Duration: int, CaloriesBurned: float):
+    def __init__(self, ExerciseType: str, Duration: int, CaloriesBurned: float = 0):
         self.__ExerciseType = ExerciseType
         self.__Duration = Duration
         self.__CaloriesBurned = CaloriesBurned
 
-    def Constructor(self, ExerciseType, Duration, CaloriesBurned = 0):
-        pass
+
     def GetExerciseType(self):
         return self.__ExerciseType
+    
     def GetDuration(self):
         return self.__Duration
+    
     def GetCaloriesBurned(self):
         return self.__CaloriesBurned
+
+    def CalculateCalories(self, ExerciseType, Duration):
+        if self.__ExerciseType == 'Running':
+            self.__CaloriesBurned = self.__Duration * 10
+        elif self.__ExerciseType == 'Swimming':
+            self.__CaloriesBurned = self.__Duration * 8
+        elif self.__ExerciseType == 'Walking':
+            self.__CaloriesBurned = self.__Duration * 5
+        else:
+            self.__CaloriesBurned = self.__Duration * 6
