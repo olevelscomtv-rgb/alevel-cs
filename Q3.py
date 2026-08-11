8eca34cfa9d3e4dc142959c7341ddfe78ead855a
TaahaTm <taahatahir.m@gmail.com>
2026-08-11 13:40:24 +0300

    Q3 part a2

--- Q3.py
+++ Q3.py
@@ -1,8 +1,14 @@
 class Excercise:
     def __init__(self, ExerciseType: str, Duration: int, CaloriesBurned: float):
-        self.ExerciseType = ExerciseType
-        self.Duration = Duration
-        self.CaloriesBurned = CaloriesBurned
+        self.__ExerciseType = ExerciseType
+        self.__Duration = Duration
+        self.__CaloriesBurned = CaloriesBurned
 
     def Constructor(self, ExerciseType, Duration, CaloriesBurned = 0):
         pass
+    def GetExerciseType(self):
+        return self.__ExerciseType
+    def GetDuration(self):
+        return self.__Duration
+    def GetCaloriesBurned(self):
+        return self.__CaloriesBurned
