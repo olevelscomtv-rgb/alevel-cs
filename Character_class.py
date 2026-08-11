4c826cdd456d7026ff6746fc99dba7973576793b
TaahaTm <taahatahir.m@gmail.com>
2026-08-11 14:35:10 +0300

    part a

---
+++ Character_class.py
@@ -0,0 +1,5 @@
+class Character:
+    def __init__(self, Name, Xcoordinate, Ycoordinate): #(Name: str, Xcoordinate: int, Ycoordinate: int)
+        self.Name = Name
+        self.Xcoordinate = Xcoordinate
+        self.Ycoordinate = Ycoordinate
