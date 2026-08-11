873aec4a43a19de9bc747636284ec04009add41f
TaahaTm <taahatahir.m@gmail.com>
2026-08-11 14:38:47 +0300

    Q2 part b

--- Character_class.py
+++ Character_class.py
@@ -1,5 +1,14 @@
 class Character:
-    def __init__(self, Name, Xcoordinate, Ycoordinate): #(Name: str, Xcoordinate: int, Ycoordinate: int)
-        self.Name = Name
-        self.Xcoordinate = Xcoordinate
-        self.Ycoordinate = Ycoordinate
+    def __init__(self, Name, XCoordinate, YCoordinate): #(Name: str, XCoordinate: int, YCoordinate: int)
+        self.__Name = Name
+        self.__XCoordinate = XCoordinate
+        self.__YCoordinate = YCoordinate
+
+    def GetName(self):
+        return self.__Name
+
+    def GetX(self):
+            return self.__XCoordinate
+
+    def GetY(self):
+            return self.__YCoordinate
