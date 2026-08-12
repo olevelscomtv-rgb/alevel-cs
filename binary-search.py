bed259bb68ad479bf867d4c9f52ca93b624ee580
TaahaTm <taahatahir.m@gmail.com>
2026-08-12 14:25:32 +0300

    binary_search

---
+++ binary-search.py
@@ -0,0 +1,17 @@
+def binary_search(array, value):
+    left = 0
+    right = len(array)-1
+
+    while left <= right:
+        middle = (left+right)//2
+
+        if array(middle) == value:
+            return middle
+
+        elif array(middle) > value:
+            right = middle - 1
+
+        else:
+            left = middle + 1
+
+    return -1
