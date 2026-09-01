942039f833b9017a662c35c1fe6d8641fa873fac
TaahaTm <taahatahir.m@gmail.com>
2026-09-01 08:47:37 +0300

    q3

--- T3-Q3.py
+++ T3-Q3.py
@@ -25,3 +25,20 @@ class Character:
 FirstCharacter = Character("Royal", "2019-1-19", 70, 30)
 FirstCharacter.Learn()
 print(f"Character Name: {FirstCharacter.GetName()}, Age: {FirstCharacter.ReturnAge()}, Intelligence: {FirstCharacter.GetIntelligence()}")
+
+class MagicCharacter(Character):
+    def __init__(self, Element, CharacterName, DateOfBirth, Intelligence, Speed):
+        super().__init__(CharacterName, DateOfBirth, Intelligence, Speed)
+        self.__Element = Element #STRING
+
+    def Learn(self):
+        if self.__Element == "Fire" or self.__Element == "Water":
+            self.SetIntelligence(self.GetIntelligence() * 1.2)
+        elif self.__Element == "Earth":
+            self.SetIntelligence(self.GetIntelligence() * 1.3)
+        else:
+            self.SetIntelligence(self.GetIntelligence() * 1.1)
+
+FirstMagic = MagicCharacter("Fire", "Light", "2018-3-3", 75, 22)
+FirstMagic.Learn()
+print(f"Character Name: {FirstMagic.GetName()}, Age: {FirstMagic.ReturnAge()}, Intelligence: {FirstMagic.GetIntelligence()}")
